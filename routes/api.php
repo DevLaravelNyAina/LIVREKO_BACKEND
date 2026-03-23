f18550bbda2a9e822bebfb72e774be1a004ec9f1
Ny Aina <rabenandrasananiaina@gmail.com>
2026-03-23 15:51:18 -0500

    ajout le middleware(auth:sanctum)

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,16 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::post('/login',[AuthController::class, 'login']);
 
-Route::apiResource('post',PostController::class)->except(['show']);
-Route::apiResource('user',UserController::class)->except(['show']);
+Route::post('/register',[AuthController::class,'register']);
+
+Route::middleware('auth:sanctum')->group( function(){
+
+    Route::apiResource('/post',PostController::class)->except(['show']);
+
+    Route::apiResource('/user',UserController::class)->except(['index','create','store','show']);
+
+});
