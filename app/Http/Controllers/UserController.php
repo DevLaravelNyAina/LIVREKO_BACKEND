f3794873861b10a27b8dc58328269542a698cd3b
Ny Aina <rabenandrasananiaina@gmail.com>
2026-03-23 15:47:54 -0500

    enleve les fuction (create,store)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,49 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-         return User::all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(UserFormRequest $request)
-    {
-        $validated = $request->validated();
-
-        $user = User::create([
-            'name'=>$validated['name'],
-            'email'=>$validated['email'],
-            'password'=>Hash::make($validated['password'])
-        ]);
-
-        return response()->json([
-            'message'=>'bien creer',
-            'user'=>$user
-        ]);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
+    
     /**
      * Show the form for editing the specified resource.
      */
