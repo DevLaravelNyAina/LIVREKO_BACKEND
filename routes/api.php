5867b5d3cb4238af27fe1269a00f445b1aec4a35
Ny Aina <rabenandrasananiaina@gmail.com>
2026-03-24 09:22:56 -0500

    add route logout

--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,16 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/register',[AuthController::class,'register']);
 
 Route::post('/login',[AuthController::class, 'login']);
-
-Route::post('/register',[AuthController::class,'register']);
 
 Route::middleware('auth:sanctum')->group( function(){
 
     Route::apiResource('/post',PostController::class)->except(['show']);
 
     Route::apiResource('/user',UserController::class)->except(['index','create','store','show']);
+
+    Route::post('/logout',[AuthController::class, 'logout']);
 
 });
